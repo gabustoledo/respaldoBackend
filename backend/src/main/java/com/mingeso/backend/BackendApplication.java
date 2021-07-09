65ac60a97aa1c82a06bcbf0ba8e7601a3afdc3bf
gabustoledo <gabriel.bustamante@usach.cl>
2021-07-08 22:17:50 -0400

    respaldo 2

--- backend/src/main/java/com/mingeso/backend/BackendApplication.java
+++ backend/src/main/java/com/mingeso/backend/BackendApplication.java
@@ -1,13 +1,28 @@
 package com.mingeso.backend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.mingeso.backend.uploadingfiles.storage.StorageProperties;
+import com.mingeso.backend.uploadingfiles.storage.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			//storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
